6e35fc17e1d77959593b92b18cc2c3a13d808ca2
Ridaa Nasir <ridaanasirr@outlook.com>
2024-06-05 04:32:36 +0300

    import the container folder

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import Brand from './components/brand/Brand'
 import Cta from './components/cta/Cta'
 import Feature from './components/feature/Feature'
 import Navbar from './components/navbar/Navbar'
+import Blog from './container/blog/Blog'
+import Features from './container/features/Features'
+import Footer from './container/footer/Footer'
+import Header from './container/header/Header'
+import Possibility from './container/possibility/Possibility'
+import WhatGPT3 from './container/whatGPT3/WhatGPT3'
 const App = () => {
   return (
     <div>
@@ -13,6 +19,14 @@ const App = () => {
       <Cta/>
       <Feature/>
       <Navbar/>
+
+      <Blog/>
+      <Features/>
+      <Footer/>
+      <Header/>
+      <Possibility/>
+      <WhatGPT3/>
+
     </div>
   )
 }
