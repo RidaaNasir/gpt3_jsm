70ec2999164940c068c1c7cfa511c31137b18c57
Ridaa Nasir <ridaanasirr@outlook.com>
2024-07-24 18:42:34 +0300

    done hosting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Updated import for React 18
-
-import App from './App';
+import ReactDOM  from "react-dom";
+import App from './App'
 import './index.css'
-
-const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root
-root.render(<App />); // Use the new render method
+//import { Component } from "./Components";
+ReactDOM.render(<App />,document.querySelector("#root"))
